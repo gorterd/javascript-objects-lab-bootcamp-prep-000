35fdca2e38749d5c885e7bba90de309484ad3ac9
Daniel Gorter <danieljgorter@gmail.com>
2019-01-03 20:15:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object,{[key]:value})
 }
+
+function deleteFromObjectByKey(object, key){
+  var tempObject = Objection.assign({}, object)
+  delete tempObject[key]
+  return tempObject
+}
